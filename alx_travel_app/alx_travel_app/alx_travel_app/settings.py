66891ab82aab9eb88c24136938f1d0fa3a5c235a
Bobybuu <c43057772@gmail.com>
2025-08-21 19:40:43 +0300

    correct set up of Django project

--- alx_travel_app/alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/alx_travel_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+n%0%_k#c06f(k#)j63hxl068hd@uxnyb01^59y(rtt@&qn+k)'
+SECRET_KEY = 'django-insecure-ovqqo^^fohs9)0&houqa@8$n^3i!8w^k)i&pb94s_&*cpsnr(&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
